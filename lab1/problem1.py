d7910b7e14e836e8a602108d66e5150c17af1de0
Kabya27 <kabyahassan27@gmail.com>
2026-01-02 00:43:54 +0600

    Create problem1.py

---
+++ lab1/problem1.py
@@ -0,0 +1,8 @@
+T = int(input())
+
+for i in range(T):
+    inp = int(input())
+    if inp%2 == 0:
+        print (f"{inp} is an Even number.")
+    else:
+        print (f"{inp} is an Odd number.")
